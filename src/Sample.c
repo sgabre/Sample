53377c0453124e3347d7a1eeac458c0e476e3308
sgabre <godefroy.sieffert@laposte.net>
2016-07-23 08:54:08 +0200

    Create Repository for the Sample System Software

---
+++ src/Sample.c
@@ -0,0 +1,19 @@
+/*
+ ============================================================================
+ Name        : Sample.c
+ Author      : GSI
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	printf("Hello Word\n");
+	return 0;
+}
+
